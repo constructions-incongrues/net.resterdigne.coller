258e3c47c74d12d2335a7273565816cdfceae297
Tristan Rivoallan <tristan.rivoallan-prestataire@laposte.fr>
2019-12-04 15:53:59 +0100

    Portabilité

--- src/public/index.php
+++ src/public/index.php
@@ -9,13 +9,12 @@ $images = $finder
     ->files()
     ->in(__DIR__.'/images/oppressors');
 $images = iterator_to_array($images);
-$image = str_replace('/usr/local/src/public/','',$images[array_rand($images)]);
+$image = $images[array_rand($images)];
+$imageUrl = implode('/', array_slice(explode('/', $image->getPathName()), -3, 3));
 
 $insults = file(__DIR__.'/insults.txt');
 $insult = $insults[array_rand($insults)];
-
-exec('convert $file -gravity South -pointsize 196 -stroke black -fill "#FFFFFF" -colorspace Gray -separate -average -annotate 0 "" ./var/coller/$(basename $file)')
 ?>
 
 <p><?php echo $insult ?></p>
-<img src="<?php echo urlencode($image) ?>" style="height:100%;"/>
+<img src="images/<?php echo rawurlencode($imageUrl) ?>" style="height:100%;"/>
