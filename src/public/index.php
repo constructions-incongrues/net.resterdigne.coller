7abc9a284617b76f94fbc212db7a7d38cbb56bdc
Tristan Rivoallan <tristan.rivoallan-prestataire@laposte.fr>
2019-12-08 05:45:30 +0100

    NEXT

--- src/public/index.php
+++ src/public/index.php
@@ -16,7 +16,7 @@ $image = $images[array_rand($images)];
 
 <head>
   <meta charset="utf-8">
-  <title>Coller</title>
+  <title>Coller | Rester Digne</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
