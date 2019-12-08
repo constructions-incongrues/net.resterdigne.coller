ef31dc4e8f668b41a05d01170fa0a19e831f0145
Tristan Rivoallan <tristan.rivoallan-prestataire@laposte.fr>
2019-12-08 03:04:29 +0100

    Static

--- src/public/index.php
+++ src/public/index.php
@@ -1,54 +1,74 @@
 <?php
-
 use Symfony\Component\Finder\Finder;
 
 require_once(__DIR__.'/../vendor/autoload.php');
 
 $finder = new Finder;
-$images = $finder
+$images = iterator_to_array(
+  $finder
     ->files()
-    ->in(__DIR__.'/images/oppressors');
-$images = iterator_to_array($images);
+    ->in(__DIR__.'/coller/png')
+);
 $image = $images[array_rand($images)];
-$urlParts = array_slice(explode('/', $image->getPathName()), -3, 3);
-$urlParts[2] = rawurlencode($urlParts[2]);
-$imageUrl = implode('/', $urlParts);
-
-$insults = file(__DIR__.'/insults.txt');
-$insult = $insults[array_rand($insults)];
 ?>
-<html>
-    <head>
-        <style>
-        body {
-            background-color: #000;
-            overflow: hidden;
-            text-align: center;
-            font-family: 'Serif'
-        }
+<!doctype html>
+<html class="no-js" lang="">
 
-        #image {
-            height: 100%;
-            width: 50%;
-        }
+<head>
+  <meta charset="utf-8">
+  <title>Coller</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        #overlay {
-            color: #EEE;
-            font-size: 5em;
-            /* text-shadow: 2px 2px 2px #000; */
-            text-align: center;
-            position: absolute;
-            top: 80%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: 50%;
-            -webkit-text-stroke: 1px #777;
-        }
-        </style>
-    </head>
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
 
-    <body>
-        <p id="overlay"><?php echo $insult ?></p>
-        <img id="image" src="images/<?php echo $imageUrl ?>" />
-    </body>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+
+  <style>
+    body {
+      background-color: #000;
+    }
+    .image {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        max-width: 100%;
+        max-height: 100%;
+        margin: auto;
+        overflow: auto;
+    }
+  </style>
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+  <a target="_blank" title="IMPRIMER" href="coller/pdf/<?php echo rawurlencode(pathinfo($image->getRelativePathName())['filename']) ?>.pdf">
+    <img class="image" src="coller/png/<?php echo rawurlencode($image->getRelativePathName()) ?>"></img>
+  </a>
+
+  <script src="js/vendor/modernizr-3.7.1.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
 </html>
